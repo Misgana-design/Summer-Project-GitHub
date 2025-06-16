d8216e01693032449d57a19e12b07d2964e15516
Misgana-design <misganawoldeselassie@gmail.com>
2025-06-16 01:47:06 -0700

    I have tried to learn factory function

--- Calc Project/Calc Pr.js
+++ Calc Project/Calc Pr.js
@@ -1,1 +1,26 @@
 console.log("It is working welcome")
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 2
+    },
+    draw: function () {
+        console.log("Draw")
+    }
+}
+
+circle.draw();
+
+function createCircle(radius) {
+    return {
+        radius: radius,
+        draw: function () {
+            console.log("Factory Function");
+        }
+    }
+}
+
+const circle2 = createCircle(1);
+console.log(circle2)
